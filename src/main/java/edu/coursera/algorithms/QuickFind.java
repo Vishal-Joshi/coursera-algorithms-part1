9e361a9d29a7dfbf24f3d47658c9acf7d4b91952
vishal <joshivishal2010@gmail.com>
2018-08-10 23:32:42 +0100

    Quick find implementation

---
+++ src/main/java/edu/coursera/algorithms/QuickFind.java
@@ -0,0 +1,46 @@
+package edu.coursera.algorithms;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class QuickFind {
+
+    private static final int[] id = new int[10];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            id[i] = i;
+        }
+
+        QuickFind quickFind = new QuickFind();
+        quickFind.union(0, 5);
+        quickFind.union(5, 6);
+        quickFind.union(6, 1);
+        quickFind.union(1, 2);
+        quickFind.union(2, 7);
+
+        quickFind.union(3, 8);
+        quickFind.union(4, 3);
+        quickFind.union(9, 4);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(id[i]+" ");
+        }
+        System.out.print(quickFind.find(7, 1));
+
+
+    }
+
+    public boolean find(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+        int pid = id[p];
+        for (int i = 0; i < 10; i++) {
+            if (id[i] == pid) {
+                id[i] = id[q];
+            }
+        }
+    }
+}
